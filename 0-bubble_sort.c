4504a33ade15671bfd6b5ced6e0a0913e774c716
Muhwezi <michealphenh10@gmail.com>
2024-04-30 02:19:31 +0300

    Notation file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,17 +10,19 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 
 	if (array)
-	for (; index < size - 1; index++)
 	{
-		for (index_of_swap = 0; index_of_swap < size - index - 1; index_of_swap++)
+		for (; index < size - 1; index++)
 		{
-			if (array[index_of_swap] > array[index_of_swap + 1])
-			{
-				temp = array[index_of_swap];
-				array[index_of_swap] = array[index_of_swap + 1];
-				array[index_of_swap + 1] = temp;
-				print_array(array, size);
-			}
+			for (index_of_swap = 0; index_of_swap < size - index - 1; index_of_swap++)
+				{
+					if (array[index_of_swap] > array[index_of_swap + 1])
+						{
+							temp = array[index_of_swap];
+							array[index_of_swap] = array[index_of_swap + 1];
+							array[index_of_swap + 1] = temp;
+							print_array(array, size);
+						}
+				}
 		}
 	}
 }
