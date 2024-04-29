abc41bdbcc4e7e33ffb0acaba06c580ef5e5029c
Muhwezi <michealphenh10@gmail.com>
2024-04-30 02:05:32 +0300

    If array is empty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,7 @@ void bubble_sort(int *array, size_t size)
 	size_t index = 0, index_of_swap = 0;
 	int temp;
 
+	if (array)
 	for (; index < size - 1; index++)
 	{
 		for (index_of_swap = 0; index_of_swap < size - index - 1; index_of_swap++)
