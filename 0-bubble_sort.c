e5232ecc7340ae17bc6f8a45de75090d44e1e489
enejepromise <enejepromise@gmail.com>
2024-04-29 15:16:39 -0700

    bubble_sort update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,20 +6,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t index = 0, index_of_swap = 0;
-  int temp;
- 
-  for (; index < size - 1; index++)
-    {
-      for (index_of_swap = 0; index_of_swap < size - index - 1; index_of_swap++)
+	size_t index = 0, index_of_swap = 0;
+	int temp;
+
+	for (; index < size - 1; index++)
 	{
-	  if (array[index_of_swap] > array[index_of_swap + 1])
-	    {
-	      temp = array[index_of_swap];
-	      array[index_of_swap] = array[index_of_swap + 1];
-	      array[index_of_swap + 1] = temp;
-	      print_array(array, size);
-	    }
+		for (index_of_swap = 0; index_of_swap < size - index - 1; index_of_swap++)
+		{
+			if (array[index_of_swap] > array[index_of_swap + 1])
+			{
+				temp = array[index_of_swap];
+				array[index_of_swap] = array[index_of_swap + 1];
+				array[index_of_swap + 1] = temp;
+				print_array(array, size);
+			}
+		}
 	}
-    }
 }
